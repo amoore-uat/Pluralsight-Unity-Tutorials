1ee5db6c98d1f82b92959314d78ac13bf549a6b7
Adam <amoore@uat.edu>
2020-02-10 16:12:18 -0700

    Initial Unity Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator anim;
+    private NavMeshAgent agent;
+
+    void Awake()
+    {
+        //anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
